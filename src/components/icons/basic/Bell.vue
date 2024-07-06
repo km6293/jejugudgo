2f61d7f2cb9aed1cc79bea4549fdd254c53de437
km6293 <alstn7616@naver.com>
2024-07-06 23:40:18 +0900

    feat: icons 컴포넌트 생성

---
+++ src/components/icons/basic/Bell.vue
@@ -0,0 +1,46 @@
+<template>
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M15 11C15 9.93913 14.5786 8.92172 13.8284 8.17157C13.0783 7.42143 12.0609 7 11 7C9.93913 7 8.92172 7.42143 8.17157 8.17157C7.42143 8.92172 7 9.93913 7 11V14.5C7 14.8978 6.84196 15.2794 6.56066 15.5607C6.27936 15.842 5.89783 16 5.5 16H16.5C16.1022 16 15.7206 15.842 15.4393 15.5607C15.158 15.2794 15 14.8978 15 14.5V11Z"
+      fill="#C0FFC5"
+    />
+    <path
+      d="M10 18.5H12"
+      stroke="#09DF7A"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M10.09 7.1001C9.21019 7.30564 8.42588 7.80299 7.86479 8.51116C7.30371 9.21933 6.9989 10.0966 7 11.0001V14.5001C7 14.8979 6.84196 15.2794 6.56066 15.5607C6.27936 15.842 5.89783 16.0001 5.5 16.0001H16.5C16.1022 16.0001 15.7206 15.842 15.4393 15.5607C15.158 15.2794 15 14.8979 15 14.5001V14.0001"
+      stroke="#09DF7A"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M15.5 5.5V11.5"
+      stroke="#09DF7A"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M12.5 8.5H18.5"
+      stroke="#09DF7A"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'BellIcon',
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/icons/basic/Delete1.vue
@@ -0,0 +1,52 @@
+<template>
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M6 8.5H18"
+      stroke="#09DF7A"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M7.5 8.5H16.5V17.5C16.5 17.7652 16.3946 18.0196 16.2071 18.2071C16.0196 18.3946 15.7652 18.5 15.5 18.5H8.5C8.23478 18.5 7.98043 18.3946 7.79289 18.2071C7.60536 18.0196 7.5 17.7652 7.5 17.5V8.5Z"
+      fill="#C0FFC5"
+    />
+    <path
+      d="M7.5 8.5H16.5V17.5C16.5 17.7652 16.3946 18.0196 16.2071 18.2071C16.0196 18.3946 15.7652 18.5 15.5 18.5H8.5C8.23478 18.5 7.98043 18.3946 7.79289 18.2071C7.60536 18.0196 7.5 17.7652 7.5 17.5V8.5Z"
+      stroke="#09DF7A"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M9.5 8.5V8C9.5 7.33696 9.76339 6.70107 10.2322 6.23223C10.7011 5.76339 11.337 5.5 12 5.5C12.663 5.5 13.2989 5.76339 13.7678 6.23223C14.2366 6.70107 14.5 7.33696 14.5 8V8.5"
+      stroke="#09DF7A"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M10.5 11.5015V15.503"
+      stroke="#09DF7A"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M13.5 11.5015V15.503"
+      stroke="#09DF7A"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'Delete1Icon',
+};
+</script>
+
+<style scoped></style>
