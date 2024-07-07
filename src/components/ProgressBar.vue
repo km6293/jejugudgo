db3f7d69a1d0f021f2f3cb11ed76292df99734aa
km6293 <alstn7616@naver.com>
2024-07-07 15:33:18 +0900

    feat: progressbar component 추가

---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div
+    class="progress-bar"
+    :style="{ width: `${progress}%` }"
+  ></div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+
+export default defineComponent({
+  name: 'ProgressBar',
+  props: {
+    progress: {
+      type: Number as PropType<number>,
+      required: true,
+      validator: (value: number) => value >= 0 && value <= 100,
+    },
+  },
+});
+</script>
+
+<style scoped lang="scss">
+.progress-bar {
+  height: 4px;
+  background-color: $color-green-300;
+  transition: width 0.3s;
+}
+</style>
