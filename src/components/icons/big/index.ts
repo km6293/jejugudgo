1baefcf74a2e9e3cc3436b8c1f4cb6b31e20150d
km6293 <alstn7616@naver.com>
2024-07-19 22:52:28 +0900

    feat: 48px main icon 추가

---
+++ src/components/icons/big/index.ts
@@ -0,0 +1,5 @@
+import Arrow from './Arrow.vue';
+import Delete from './Delete.vue';
+import Search from './Search.vue';
+
+export { Arrow as ArrowIcon, Delete as DeleteIcon, Search as SearchIcon };
