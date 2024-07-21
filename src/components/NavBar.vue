9e2cfbeed74cb5b208e7c31e414aa261b25ee897
km6293 <alstn7616@naver.com>
2024-07-22 00:50:09 +0900

    feat: navbar icon 추가

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,15 +1,78 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/login">Login</router-link>
+    <router-link
+      to="/"
+      class="nav-item"
+      :class="{ active: isActive('/') }"
+    >
+      <div class="nav-icon">
+        <component :is="isActive('/') ? HomeActiveIcon : HomeDisabledIcon" />
+        <span>홈</span>
+      </div>
+    </router-link>
+    <router-link
+      to="/about"
+      class="nav-item"
+      :class="{ active: isActive('/about') }"
+    >
+      <div class="nav-icon">
+        <component
+          :is="
+            isActive('/about')
+              ? CourseSearchActiveIcon
+              : CourseSearchDisabledIcon
+          "
+        />
+        <span>코스검색</span>
+      </div>
+    </router-link>
+    <router-link
+      to="/walk-together"
+      class="nav-item"
+      :class="{ active: isActive('/walk-together') }"
+    >
+      <div class="nav-icon">
+        <component
+          :is="
+            isActive('/walk-together')
+              ? WalkTogetherActiveIcon
+              : WalkTogetherDisabledIcon
+          "
+        />
+        <span>같이걷기</span>
+      </div>
+    </router-link>
+    <router-link
+      to="/my-walk"
+      class="nav-item"
+      :class="{ active: isActive('/my-walk') }"
+    >
+      <div class="nav-icon">
+        <component
+          :is="isActive('/my-walk') ? MyWalkGoActiveIcon : MyWalkGoDisabledIcon"
+        />
+        <span>마이걷고</span>
+      </div>
+    </router-link>
   </nav>
 </template>
 
-<script>
-export default {
-  name: 'NavBar',
-};
+<script setup>
+import { useRoute } from 'vue-router';
+import {
+  CourseSearchActiveIcon,
+  CourseSearchDisabledIcon,
+  HomeActiveIcon,
+  HomeDisabledIcon,
+  MyWalkGoActiveIcon,
+  MyWalkGoDisabledIcon,
+  WalkTogetherActiveIcon,
+  WalkTogetherDisabledIcon,
+} from '@/components/icons/navigation';
+
+const route = useRoute();
+
+const isActive = (path) => route.path === path;
 </script>
 
 <style scoped>
@@ -19,24 +82,36 @@ nav {
   bottom: 0;
   width: 100%;
   background-color: #333;
-  padding: 15px 0 15px 0;
+  padding: 16px 0;
   text-align: center;
   min-width: 280px;
   max-width: 768px;
+  display: flex;
+  justify-content: space-around;
+}
 
-  a {
-    font-weight: bold;
-    color: #fff;
-    text-decoration: none;
-    margin: 0 10px;
+.nav-item {
+  display: flex;
+  /* flex-direction: column; */
+  align-items: center;
+  text-decoration: none;
+  color: var(--color-neutral-500);
+}
 
-    &:hover {
-      color: #42b983;
-    }
+.nav-item.active {
+  color: var(--color-neutral-100);
+  font-family: var(--font-regular);
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.nav-item span {
+  margin-top: 5px;
+}
+
+.nav-icon {
+  height: 48px;
+  width: 48px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 </style>
