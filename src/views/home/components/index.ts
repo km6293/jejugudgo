30eaf27f22106aee64e1c4a2ceea3331e1154913
km6293 <alstn7616@naver.com>
2024-08-21 20:16:55 +0900

    feat: 제주소식 컴포넌트 추가

---
+++ src/views/home/components/index.ts
@@ -0,0 +1,7 @@
+import ContestCard from './ContestCard.vue';
+import Header from './Header.vue';
+import RecommendedCard from './RecommendedCard.vue';
+import Title from './Title.vue';
+import Advertisement from './Advertisement.vue';
+
+export { ContestCard, Header, Title, RecommendedCard, Advertisement };
