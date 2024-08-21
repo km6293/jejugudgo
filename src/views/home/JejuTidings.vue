19731857c488b4f40d3fa45a484842b451a259e1
km6293 <alstn7616@naver.com>
2024-08-21 20:15:58 +0900

    feat: 제주소식 디자인

---
+++ src/views/home/JejuTidings.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="jeju-tidings-container">
+    <div class="jeju-tidings-content">
+      <Title
+        :title="'제주도 걷기 대회'"
+        :allView="true"
+      />
+      <div class="contest-slider">
+        <div
+          v-for="item of 5"
+          :key="item"
+        >
+          <ContestCard />
+        </div>
+      </div>
+    </div>
+    <div class="jeju-tidings-content">
+      <Title
+        :title="'현지인 추천 장소'"
+        :allView="true"
+      />
+      <span
+        v-for="item of 5"
+        :key="item"
+      >
+        <RecommendedCard />
+      </span>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Title, ContestCard, RecommendedCard } from './components';
+</script>
+
+<style scoped>
+.jeju-tidings-container {
+  padding: 0px 20px 0px 20px;
+  display: flex;
+  margin-top: 40px;
+  flex-direction: column;
+  gap: 40px;
+  position: relative;
+}
+
+.jeju-tidings-content {
+  gap: var(--margin-l);
+  display: grid;
+}
+
+.contest-slider {
+  overflow-x: auto;
+  display: inline-flex;
+  gap: var(--margin-m);
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+}
+</style>
