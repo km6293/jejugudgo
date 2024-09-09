85bb44e3f110c049fb4fd19a1b73ad8fe0f5e4f5
km6293 <alstn7616@naver.com>
2024-09-09 17:51:20 +0900

    feat: 코스 지도 컴포넌트 및 아이콘

---
+++ src/components/icons/infographic/index.ts
@@ -0,0 +1,9 @@
+import Start from './Start.vue';
+import End from './End.vue';
+import Waypoint from './Waypoint.vue';
+
+export {
+  Start as StartMapIcon,
+  End as EndMapIcon,
+  Waypoint as WaypointMapIcon,
+};
