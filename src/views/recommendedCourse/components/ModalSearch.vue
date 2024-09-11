326ca4b74c76dfa541ad5e8a0af087903b858209
km6293 <alstn7616@naver.com>
2024-09-11 22:23:42 +0900

    feat: modal search 컴포넌트 추가

---
+++ src/views/recommendedCourse/components/ModalSearch.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="input-container">
+    <input class="content-input body2-medium" />
+    <SearchIcon @click="onSearchClick" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { SearchIcon } from '@/components';
+import { defineEmits } from 'vue';
+
+const emits = defineEmits(['search-click']);
+
+const onSearchClick = () => emits('search-click');
+</script>
+
+<style scoped>
+.input-container {
+  padding: 20px;
+  width: 100%;
+  height: 52px;
+  gap: var(--margin-s);
+  display: flex;
+  align-items: center;
+  border-radius: var(--radius-s);
+  background-color: var(--color-neutral-700);
+}
+
+.content-input {
+  width: 100%;
+  height: 52px;
+  border: none;
+  background-color: var(--color-neutral-700);
+  outline: none;
+  border-radius: var(--radius-s);
+  color: var(--color-text-active);
+}
+</style>
