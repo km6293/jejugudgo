9fd6fa04ce210ae46ba3d9d2a290198378f9d85e
km6293 <alstn7616@naver.com>
2024-09-11 22:17:27 +0900

    fix: 중복 코드 정리

--- src/views/recommendedCourse/CreateCourse.vue
+++ src/views/recommendedCourse/CreateCourse.vue
@@ -1,37 +1,67 @@
 <template>
   <div class="container">
+    <MapSearch
+      v-if="courseData.ShowSearch"
+      @arrow-click="() => updateCourseData({ ShowSearch: false })"
+      :courseData="courseData"
+      :updateCourseData="updateCourseData"
+    />
     <div id="map_div"></div>
     <SwipeModal>
-      <StartCourse
-        v-if="activeStep === 1"
-        :setStep="setStep"
-      />
-      <HelperCourse
-        v-if="activeStep === 2"
-        :setStep="setStep"
-      />
-      <HelperResult
-        v-if="activeStep === 3"
-        :setStep="setStep"
+      <component
+        v-if="componentMap[courseData.page]"
+        :is="componentMap[courseData.page]"
+        :courseData="courseData"
+        :updateCourseData="updateCourseData"
       />
     </SwipeModal>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, reactive, defineComponent } from 'vue';
 import { useMap, useLocation } from '@/hooks';
 import { SwipeModal } from '@/components';
-import { StartCourse, HelperCourse, HelperResult } from './components';
+import {
+  StartPoint,
+  SuggestionHelper,
+  ResultHelper,
+  WayPoint,
+  SelectKeyword,
+  CheckPublic,
+  CompleteCourse,
+  MapSearch,
+} from './components';
+import { ICourseDataType } from './type';
 
 const map = ref<any>(null);
 
 const { initTmap } = useMap(map);
 const { moveNowLocation } = useLocation(map);
 
-const activeStep = ref(1);
-const setStep = (step: number) => {
-  activeStep.value = step;
+const courseData = reactive<ICourseDataType>({
+  input: '',
+  page: 1,
+  ShowSearch: false,
+  spot: [],
+  visibilityStatus: true,
+  courseName: '',
+  keyword: [],
+});
+
+const componentMap: { [key: number]: ReturnType<typeof defineComponent> } = {
+  1: StartPoint,
+  2: WayPoint,
+  3: SelectKeyword,
+  4: CheckPublic,
+  5: CompleteCourse,
+  11: SuggestionHelper,
+  12: ResultHelper,
+};
+
+const updateCourseData = (newData: Partial<ICourseDataType>) => {
+  Object.assign(courseData, newData);
+  console.log(courseData);
 };
 
 onMounted(async () => {
