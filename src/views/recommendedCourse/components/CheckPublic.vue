6b92a04f844d3f2eb6550d90c8f1e82b98a997c0
km6293 <alstn7616@naver.com>
2024-09-11 22:19:44 +0900

    feat: 공개 여부 페이지

---
+++ src/views/recommendedCourse/components/CheckPublic.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="container">
+    <ModalHeader
+      title="코스를 공개하시겠어요?"
+      @left-click="updateData('page', 3)"
+      @right-click="updateData('page', 5)"
+    />
+    <div class="check-content">
+      <div class="button-container body2-regular">
+        <div class="check-button">공개 할래요</div>
+        <div class="check-button">공개 안할래요</div>
+      </div>
+      <div class="check-info caption-medium">
+        코스를 공개하면 다른 유저들도 코스를 이용할 수 있어요
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+import { ICourseDataType } from '../type';
+import { ModalHeader } from './index';
+
+const props = defineProps<{
+  updateCourseData: (newData: Partial<ICourseDataType>) => void;
+  courseData: ICourseDataType;
+}>();
+
+const updateData = <K extends keyof ICourseDataType>(
+  item: K,
+  value: ICourseDataType[K]
+) => {
+  props.updateCourseData({ [item]: value });
+};
+</script>
+
+<style scoped lang="scss">
+.container {
+  padding: 0 20px;
+  display: flex;
+  flex-direction: column;
+  gap: var(--margin-m);
+}
+
+.check-content {
+  display: flex;
+  flex-direction: column;
+  gap: var(--margin-s);
+}
+
+.button-container {
+  width: 100%;
+  display: flex;
+  gap: var(--margin-s);
+}
+.check-button {
+  height: 48px;
+  padding: var(--padding-m);
+  border-radius: var(--radius-s);
+  border: 1px solid var(--Neutral-700, #555555);
+  display: flex;
+  align-items: center;
+  color: var(--color-button-on-secondary);
+  flex: 1;
+  justify-content: center;
+
+  &:active {
+    color: var(--color-text-primary);
+    border-color: var(--color-text-primary);
+  }
+}
+
+.check-info {
+  color: var(--color-text-muted);
+}
+</style>
