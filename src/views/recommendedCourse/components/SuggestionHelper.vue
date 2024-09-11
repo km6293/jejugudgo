c8058528deef290f5baa56c255ecd2457e91518b
km6293 <alstn7616@naver.com>
2024-09-11 22:22:28 +0900

    fix: 제안 페이지 검색 컴포넌트 정리

--- src/views/recommendedCourse/components/SuggestionHelper.vue
+++ src/views/recommendedCourse/components/SuggestionHelper.vue
@@ -1,22 +1,10 @@
 <template>
   <div class="container">
-    <div class="header">
-      <div class="start-text">
-        <div class="start-title subheading-bold">
-          <BigArrowIcon @click="movePage(1)" />
-          어떤 장르의 코스를 원하세요?
-        </div>
-      </div>
-      <div class="button">
-        <Button
-          @click="movePage(3)"
-          text="다음"
-          :style="{
-            height: '40px',
-          }"
-        />
-      </div>
-    </div>
+    <ModalHeader
+      title="어떤 장르의 코스를 원하세요?"
+      @left-click="updateData('page', 1)"
+      @right-click="updateData('page', 12)"
+    />
     <div class="content">
       <div class="slider">
         <div
@@ -38,15 +26,21 @@
 </template>
 
 <script setup lang="ts">
-import { Button, BigArrowIcon, CardImage } from '@/components';
+import { CardImage } from '@/components';
 import { defineProps } from 'vue';
+import { ICourseDataType } from '../type';
+import { ModalHeader } from './index';
 
 const props = defineProps<{
-  setStep: (step: number) => void;
+  updateCourseData: (newData: Partial<ICourseDataType>) => void;
+  courseData: ICourseDataType;
 }>();
 
-const movePage = (num: number) => {
-  props.setStep(num);
+const updateData = <K extends keyof ICourseDataType>(
+  item: K,
+  value: ICourseDataType[K]
+) => {
+  props.updateCourseData({ [item]: value });
 };
 </script>
 
@@ -56,24 +50,6 @@ const movePage = (num: number) => {
   display: flex;
   flex-direction: column;
   gap: var(--margin-m);
-}
-
-.header {
-  align-items: center;
-  display: flex;
-  justify-content: space-between;
-}
-
-.start-text {
-  display: flex;
-  gap: var(--margin-xs);
-  align-items: center;
-}
-
-.start-title {
-  display: flex;
-  align-items: center;
-  color: #f2f2f2;
 }
 
 .slider {
