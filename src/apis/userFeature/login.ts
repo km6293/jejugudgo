4692328204917c5a8a9ad70771730828ca148f48
Kim Woo Seong <112603076+dntjd12301@users.noreply.github.com>
2024-09-12 10:48:06 +0900

    Login/OuthLogin Add

--- src/apis/userFeature/login.ts
+++ src/apis/userFeature/login.ts
@@ -1,17 +1,19 @@
-import axiosInstance from '@/apis';
+import axios from 'axios';
 import { setCookie, hasError } from '@/utils';
 import { ILoginRequest } from '@/types/api';
 
 export const login = async (data: ILoginRequest) => {
   try {
-    const response = await axiosInstance.post(`/auth/login`, data);
+    const url = `${process.env.VUE_APP_BASE_API}/api/v1/auth/login`;
+    const response = await axios.post(url, data);
+
     const accessToken = response.headers['authorization'];
 
     if (accessToken) {
       setCookie('Authorization', accessToken);
     }
 
-    return response.data;
+    return response.status;
   } catch (error) {
     return hasError(error);
   }
