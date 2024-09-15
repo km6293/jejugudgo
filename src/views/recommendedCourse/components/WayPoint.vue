af49567fee8b7a23e3c6d1270fa84e0c139bcd75
km6293 <alstn7616@naver.com>
2024-09-15 14:20:59 +0900

    fix: startPoint input waypoint에 이동

--- src/views/recommendedCourse/components/WayPoint.vue
+++ src/views/recommendedCourse/components/WayPoint.vue
@@ -3,13 +3,47 @@
     <ModalHeader
       title="다음 지점을 선택해주세요"
       :showInfoIcon="true"
-      @left-click="updateData('page', 1)"
-      @right-click="updateData('page', 3)"
+      :showLeftIcon="false"
+      :rightDisabled="!areWaypointsAndEndPointValid"
     />
 
     <div class="content">
       <div class="location">
-        <ModalSearch />
+        <ModalSearch
+          :point="startPoint"
+          @click="handleCheckPointClick({ type: 'startPoint' })"
+        />
+
+        <div
+          v-for="(wayPoint, index) in wayPoint"
+          :key="index"
+          class="button-container"
+        >
+          <ModalSearch
+            :point="wayPoint"
+            @click="handleCheckPointClick({ type: 'wayPoint', index })"
+          />
+          <div
+            class="button-icon"
+            @click="createCourseStore.removeWayPoint(index)"
+          >
+            <SubtractIcon />
+          </div>
+        </div>
+
+        <div class="button-container">
+          <ModalSearch
+            :point="endPoint"
+            @click="handleCheckPointClick({ type: 'endPoint' })"
+          />
+          <div
+            class="button-icon"
+            :class="{ disabled: !allWayPointsFilled }"
+            @click="handleAddWayPointClick"
+          >
+            <AddIcon />
+          </div>
+        </div>
         <div class="target">
           <TargetIcon />
           <div class="body2-regular">현위치로 설정</div>
@@ -20,7 +54,7 @@
         <div class="helper-text body1-medium">어디서로 갈지 고민된다면?</div>
         <div
           class="helper-button body2-bold"
-          @click="updateData('page', 21)"
+          @click="createCourseStore.updateData('page', 11)"
         >
           추천받기
         </div>
@@ -30,30 +64,73 @@
 </template>
 
 <script setup lang="ts">
-import { TargetIcon } from '@/components';
+import { TargetIcon, AddIcon, SubtractIcon } from '@/components';
 import { ModalSearch, ModalHeader } from './index';
-import { defineProps } from 'vue';
-import { ICourseDataType } from '../type';
+import { useCreateCourseStore } from '@/stores/recommendedCourse/createCourse';
+import { computed } from 'vue';
+import { storeToRefs } from 'pinia';
+import { IselectedObjectType } from '@/stores/recommendedCourse/type';
 
-const props = defineProps<{
-  updateCourseData: (newData: Partial<ICourseDataType>) => void;
-  courseData: ICourseDataType;
-}>();
+const createCourseStore = useCreateCourseStore();
+const { wayPoint, endPoint, startPoint } = storeToRefs(createCourseStore);
 
-const updateData = <K extends keyof ICourseDataType>(
-  item: K,
-  value: ICourseDataType[K]
-) => {
-  props.updateCourseData({ [item]: value });
+const handleAddWayPointClick = () => {
+  if (allWayPointsFilled.value) {
+    createCourseStore.addWayPoint();
+  }
+};
+
+const allWayPointsFilled = computed(() =>
+  createCourseStore.wayPoint.every(
+    (point) => point.name && point.longitude && point.latitude
+  )
+);
+
+const isEndPointValid = computed(
+  () =>
+    createCourseStore.endPoint.name &&
+    createCourseStore.endPoint.longitude &&
+    createCourseStore.endPoint.latitude
+);
+
+const areWaypointsAndEndPointValid = computed<boolean>(() => {
+  const isValid = !!(allWayPointsFilled.value && isEndPointValid.value);
+  return isValid;
+});
+
+const handleCheckPointClick = (selectedObject: IselectedObjectType) => {
+  createCourseStore.updateData('selectedObject', selectedObject);
 };
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 .container {
   padding: 0 20px;
   display: flex;
   flex-direction: column;
   gap: var(--margin-m);
+}
+
+.button-container {
+  display: flex;
+  align-content: center;
+  gap: 8px;
+}
+
+.button-icon {
+  justify-content: center;
+  align-content: center;
+  display: grid;
+  width: 48px;
+  height: 52px;
+  padding: var(--padding-m);
+  gap: var(--spacing-1);
+  border-radius: var(--padding-s);
+  background-color: var(--color-button-secondary);
+
+  &.disabled {
+    background-color: var(--color-neutral-700);
+  }
 }
 
 .location {
