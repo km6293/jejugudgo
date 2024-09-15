b7ca119fe6a49f312d718863aa8803d20eb6a411
km6293 <alstn7616@naver.com>
2024-09-15 14:17:08 +0900

    feat: 위치 정보 고정밀 센서 옵션 추가 및 현위치 icon 추가

--- src/hooks/useLocation.ts
+++ src/hooks/useLocation.ts
@@ -1,3 +1,5 @@
+import { iconMap } from '@/utils/iconMap';
+
 export const useLocation = (mapRef: any) => {
   const Tmapv2 = (window as any).Tmapv2;
 
@@ -16,18 +18,31 @@ export const useLocation = (mapRef: any) => {
         reject(err);
       };
 
-      navigator.geolocation.getCurrentPosition(success, error);
+      navigator.geolocation.getCurrentPosition(success, error, {
+        enableHighAccuracy: true,
+        timeout: 10000,
+        maximumAge: 0,
+      });
     });
   };
 
   const moveLocation = (
-    mapRef: any,
-    [latitude, longitude]: [number, number]
+    latitude: number | string,
+    longitude: number | string
   ) => {
     if (mapRef.value) {
-      const currentLocation = new Tmapv2.LatLng(latitude, longitude);
-      mapRef.value.setCenter(currentLocation);
-      mapRef.value.setZoom(15);
+      const lat =
+        typeof latitude === 'string' ? parseFloat(latitude) : latitude;
+      const lng =
+        typeof longitude === 'string' ? parseFloat(longitude) : longitude;
+
+      if (!isNaN(lat) && !isNaN(lng)) {
+        const currentLocation = new Tmapv2.LatLng(lat, lng);
+        mapRef.value.setCenter(currentLocation);
+        mapRef.value.setZoom(15);
+      } else {
+        console.error('유효하지 않은 좌표:', latitude, longitude);
+      }
     } else {
       console.error('맵 초기화가 되지 않음', 2);
     }
@@ -42,12 +57,20 @@ export const useLocation = (mapRef: any) => {
     let latitude, longitude;
     try {
       [latitude, longitude] = await findNowLocation();
+
+      const markerPosition = new Tmapv2.LatLng(latitude, longitude);
+
+      const marker = new Tmapv2.Marker({
+        position: markerPosition,
+        icon: iconMap['now'],
+        map: mapRef.value,
+      });
     } catch (error) {
       [latitude, longitude] = [33.50712081595116, 126.49340629577637];
       console.error('현재 위치를 찾을 수 없습니다:', error);
     }
 
-    moveLocation(mapRef, [latitude, longitude]);
+    moveLocation(latitude, longitude);
   };
 
   return {
