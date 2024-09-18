8f8d925fffc6eb62f09a5f882c5c15f578931579
km6293 <alstn7616@naver.com>
2024-09-19 03:08:27 +0900

    feat: android app test

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.github.jejugudgo.frontend',
+  appName: 'frontend',
+  webDir: 'dist',
+};
+
+export default config;
