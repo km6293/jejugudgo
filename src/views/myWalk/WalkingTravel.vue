da0c7930e568bfb7f82be95c234fcb58e1e36d61
km6293 <alstn7616@naver.com>
2024-09-19 23:26:09 +0900

    feat: 나의 걷기 기록 페이지

---
+++ src/views/myWalk/WalkingTravel.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <TopBar>
+      <template #center> 나의 걷기 기록 </template>
+      <template #right></template>
+    </TopBar>
+    <div id="map_div"></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { TopBar } from './components';
+import { onMounted, ref } from 'vue';
+import { useMap } from '@/hooks';
+
+const map = ref<any>(null);
+const { initTmap, addMarker, moveNowLocation } = useMap(map);
+
+onMounted(async () => {
+  await initTmap(map);
+  await moveNowLocation();
+});
+</script>
+
+<style scoped>
+.container {
+  width: 100%;
+  height: 100%;
+  position: relative;
+}
+</style>
