6bf1b3847031ea715b494dff826b760920c9d73b
km6293 <alstn7616@naver.com>
2024-09-21 05:27:47 +0900

    feat: Big Star Icon 추가

--- src/components/icons/big/index.ts
+++ src/components/icons/big/index.ts
@@ -1,9 +1,11 @@
 import Arrow from './Arrow.vue';
 import Delete from './Delete.vue';
 import Search from './Search.vue';
+import Star from './Star.vue';
 
 export {
   Arrow as BigArrowIcon,
   Delete as BigDeleteIcon,
   Search as BigSearchIcon,
+  Star as BigStarIcon,
 };
