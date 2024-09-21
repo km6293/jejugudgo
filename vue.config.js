2b05bbd8b9452a8a44bc283890a4dac1ac950608
Kim Woo Seong <112603076+dntjd12301@users.noreply.github.com>
2024-09-21 20:03:07 +0900

    의존성 추가

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,15 @@
 const { defineConfig } = require('@vue/cli-service');
+const path = require('path');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
+    },
+  },
   devServer: {
     port: 5173,
     historyApiFallback: true,
